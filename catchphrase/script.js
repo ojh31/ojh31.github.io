54287074ed04c71399b8567371f1870afb441a31
Oskar John Hollinsworth <oskar.hollinsworth@gmail.com>
2016-09-13 16:41:46 +0100

    bottom margin increased

--- catchphrase/script.js
+++ catchphrase/script.js
@@ -130,7 +130,7 @@ $(document).ready(function(){
     });
     $( "#amount" ).val($( "#durationRange" ).slider( "values", 0 ) + "s - " + 
     			       $( "#durationRange" ).slider( "values", 1 ) + "s" );
-    var margin = {top: 50, right: 50, bottom: 30, left: 50},
+    var margin = {top: 50, right: 50, bottom: 50, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
     var x = d3.scale.linear()
