999663d16e11b1d1198c64c6b9901205e70a1bf1
Oskar John Hollinsworth <oskar.hollinsworth@gmail.com>
2016-09-13 15:15:58 +0100

    bl.ocks code added

--- catchphrase/script.js
+++ catchphrase/script.js
@@ -187,53 +187,65 @@ var normrv = function(a, b, mu, sigma){
 	}	
 };
 
-var m = [80, 80, 80, 80]; // margins
-var w = 1000 - m[1] - m[3]; // width
-var h = 400 - m[0] - m[2]; // height
-// create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
-var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
-// X scale will fit all values from data[] within pixels 0-w
-var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
-// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
-// automatically determining max range can work something like this
-// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
-// create a line function that can convert data[] into x and y points
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatDate = d3.time.format("%d-%b-%y");
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
 var line = d3.svg.line()
-	// assign the X function to plot our line as we wish
-	.x(function(d,i) { 
-		// verbose logging to show what's actually being done
-		console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-		// return the X coordinate where we want to plot this datapoint
-		return x(i); 
-	})
-	.y(function(d) { 
-		// verbose logging to show what's actually being done
-		console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-		// return the Y coordinate where we want to plot this datapoint
-		return y(d); 
-	})
-	// Add an SVG element with the desired dimensions and margin.
-	var graph = d3.select("#graph").append("svg:svg")
-	      .attr("width", w + m[1] + m[3])
-	      .attr("height", h + m[0] + m[2])
-	    .append("svg:g")
-	      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-	// create yAxis
-	var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-	// Add the x-axis.
-	graph.append("svg:g")
-	      .attr("class", "x axis")
-	      .attr("transform", "translate(0," + h + ")")
-	      .call(xAxis);
-	// create left yAxis
-	var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-	// Add the y-axis to the left
-	graph.append("svg:g")
-	      .attr("class", "y axis")
-	      .attr("transform", "translate(-25,0)")
-	      .call(yAxisLeft);
-	
-  	// Add the line by appending an svg:path element with the data line we created above
-	// do this AFTER the axes above so that the line is above the tick-lines
-  	graph.append("svg:path").attr("d", line(data));
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data.tsv", type, function(error, data) {
+  if (error) throw error;
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.close; }));
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Price ($)");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+});
+
+function type(d) {
+  d.date = formatDate.parse(d.date);
+  d.close = +d.close;
+  return d;
+}
