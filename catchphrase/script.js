4cd1f457a980fd6c5f633a578843291dec495794
Oskar John Hollinsworth <oskar.hollinsworth@gmail.com>
2016-09-13 16:31:18 +0100

    xlabel moved right

--- catchphrase/script.js
+++ catchphrase/script.js
@@ -159,6 +159,7 @@ $(document).ready(function(){
         .call(xAxis)
         .attr("transform", "translate(0," + height + ")")
         .append("text")
+        .attr("transform", "translate(" + width + ",)")
         .attr("x", 6)
         .attr("dx", ".71em")
         .style("text-anchor", "start")
