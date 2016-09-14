3d987069cb9edac7c8806b7e99917040bd5c5727
Oskar John Hollinsworth <oskar.hollinsworth@gmail.com>
2016-09-14 01:43:18 +0100

    Create phrases.js

---
+++ catchphrase/phrases.js
@@ -0,0 +1,31 @@
+//upload file callback
+
+var uploadFile = function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function(){
+    	var text = reader.result;
+    	phrasesLeft = phrasesLeft.concat(text.split("\n"));
+    	phrasesTotal = phrasesTotal.concat(text.split("\n"));
+    	localStorage.setItem('savedPhrases', JSON.stringify(phrasesTotal));
+    };
+    reader.readAsText(input.files[0]);
+    $("#phrases").html(phrasesTotal.reverse().slice(0,20).join("<br>"));
+};
+
+// listen for added phrase
+
+$(document).ready(function(){
+    $("#phrases").html(phrasesTotal.reverse().slice(0,20).join("<br>"));
+    $("#newPhrase").keyup(function(key){
+		if (key.which === 13){
+			// Carriage return
+			var newPhrase = document.getElementById("newPhrase").value;
+			document.getElementById("newPhrase").value = "";
+			phrasesTotal.push(newPhrase);
+			phrasesLeft.push(newPhrase);
+			localStorage.setItem('savedPhrases', JSON.stringify(phrasesTotal));
+			 $("#phrases").html(phrasesTotal.reverse().slice(0,20).join("<br>"));
+		}
+	});
+});
