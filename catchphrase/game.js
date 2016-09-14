fe8e1d14ad0785aa8ad3f39d58227fa47df372aa
Oskar John Hollinsworth <oskar.hollinsworth@gmail.com>
2016-09-14 01:41:41 +0100

    Create game.js

---
+++ catchphrase/game.js
@@ -0,0 +1,64 @@
+function Timer(){
+	// pick random duration after which end the current game
+	this.duration = Math.random() * (maxDuration - minDuration) + minDuration;
+	console.log(this.duration);
+	this.tick = function(){
+		this.time = (Date.now() - this.startTime) / 1000;
+		$('#timer').html(this.time.toFixed(2) + " seconds");
+		$("#reset").show();
+		$("#cont").html("Continue");
+		if (this.time > this.duration || !(this.ticking)){
+			clearInterval(this.interval);
+			$("#gameMenu").hide();
+			$("#startMenu").show();
+			this.ticking = false;
+			if (this.time > this.duration){
+				$("#log").html("You ran out of time.");
+			}
+		}
+	};
+	this.stop = function(){
+		this.ticking = false;
+	};
+	this.start = function(){
+		this.startTime = Date.now();
+		this.ticking = true;
+		this.interval = setInterval(this.tick.bind(this), 10);
+	};
+}
+
+var timer;
+var cont = function(){
+	$("#startMenu").hide();
+	$("#gameMenu").show();
+	timer = new Timer();
+	timer.start();
+	next();
+};
+
+var next = function(){
+	if (phrasesLeft.length === 0){
+		$("#log").html("BOOM! You ran out of catchphrases!");
+		$("#cont").hide();
+		timer.stop();
+	}
+	var idx = Math.floor(Math.random() * phrasesLeft.length);
+	var nextPhrase = phrasesLeft.splice(idx, 1)[0];
+	$("#phrase").html(nextPhrase);
+};
+
+var reset = function(){
+	phrasesLeft = phrasesTotal.slice();
+	$("#reset").hide();
+	$("#cont").html("Play");
+	$("#cont").show();
+};
+
+$(document).ready(function(){
+    reset();
+    $("#gameMenu").hide();
+	$("#reset").hide();
+	$("#cont").click(cont);
+	$("#next").click(next);
+	$("#reset").click(reset);
+});
